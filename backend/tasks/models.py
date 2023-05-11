49863c5bf992dac930055fb70fbd2f2c40e51147
Rostislav Litvenkov <r.litvenkov@icol.com>
2023-05-11 21:01:31 +0300

    update: create Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tasks(models.Model):
+    tittle = models.CharField(max_length=50)
+    description = models.TextField()
+    is_important = models.BooleanField()
+    is_completed = models.BooleanField()
+    deadline = models.DateTimeField()
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = "tasks"
+
+    def __str__(self):
+        return self.tittle
